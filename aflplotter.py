ee8e8fd54cd22945868969c19093fb7d59fda10f
mimicria <mimicria@mail.ru>
2022-07-07 09:20:21 +0300

    add aflplotter

---
+++ aflplotter.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+import os, subprocess
+
+path = sys.argv[1]
+dirlist = [f.path for f in os.scandir(path) if f.is_dir()]
+for i in dirlist:
+    subprocess.run(['afl-plot',i, i.replace('out','plot')])
