739be19bab5f6decb7798606cfe0131e30abdea9
mimicria <gmimicria@gmail.com>
2022-12-23 13:23:11 +0300

    Create casr-collect.py

---
+++ casr-collect.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+# pip install termcolor
+
+from os import walk, path
+from sys import argv
+from termcolor import colored
+
+def get_filelist(report_dir):
+    fl = []
+    for root, dirs, files in walk(report_dir):
+    	for file in files:
+		    if(file.endswith(".casrep")):
+			    fl.append(path.join(root,file))
+    return fl
+
+def get_severity_type(report):
+    with open(report, 'r') as fd:
+        for line in fd:
+            type_pos = line.find('\"Type\":')
+            if type_pos==-1:
+                continue
+            type = line.split('\"')
+            break
+    return type[3]
+
+print(colored('CASR-collect', 'cyan'), '1.0', colored('by mimicria <mimicria@mail.ru>', 'blue'))
+print('Crash report processing utility for afl-casr')
+if len(argv) < 2:
+    print(colored('[-]', 'red'), 'Option [REPORT_DIR] is needed')    
+    exit()
+report_dir = argv[1]
+print(colored('[+]', 'green'), 'Collecting crash reports from:', colored(report_dir, 'white'))
+fl = get_filelist(report_dir)
+for file in fl:
+    type = get_severity_type(file)
+    if type == 'EXPLOITABLE':
+        color = 'red'
+    elif type == 'PROBABLY_EXPLOITABLE':
+        color = 'yellow'
+    elif type == 'NOT_EXPLOITABLE':
+        color = 'green'
+    else:
+        color = 'white'
+
+    print(file, ':', colored(type, color))
