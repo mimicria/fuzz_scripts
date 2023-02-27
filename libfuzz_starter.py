15a6ab6764d55dd08f2b535de006a2025768a777
mimicria <mimicria@mail.ru>
2023-02-27 13:58:59 +0300

    add args clear

--- libfuzz_starter.py
+++ libfuzz_starter.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
 
 import subprocess
-import time
-import re
+from time import time
+from re import search
+from sys import argv
 
-fuzz_target = '/home/user/TEST/test/TEST'
-time_limit = 7200
+if len(argv) < 3:
+    print ("Usage: python libfuzz_starter.py [path/libfuzzer_target] [time in seconds to fuzz without new cov]\n \
+  For example: python libfuzz_starter.py ~/test 7200")
+    
+    exit()
+
+fuzz_target = argv[1]
+time_limit = int(argv[2])
 
 process = subprocess.Popen(fuzz_target, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
 coverage = 0
-start_time = time.time()
+start_time = time()
 for line in process.stdout:
     print(line)
     if 'cov:' in line:
-        new_coverage = int(re.search(r'cov: (\d+)', line).group(1))
+        new_coverage = int(search(r'cov: (\d+)', line).group(1))
         if new_coverage > coverage:
             coverage = new_coverage
-            start_time = time.time()
+            start_time = time()
         else:
-            if time.time() - start_time > time_limit:
+            if time() - start_time > time_limit:
                 process.terminate()
