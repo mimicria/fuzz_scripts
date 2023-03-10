a0a9f8f66f9a4ad3ca38b32a6424f1f5957ba932
mimicria <gmimicria@gmail.com>
2023-03-10 11:32:32 +0300

    no devnull

--- ubuntu_install_fuzz.sh
+++ ubuntu_install_fuzz.sh
@@ -7,38 +7,38 @@ if [ "$EUID" -ne 0 ]
 fi
 
 echo "- Installing AFLplusplus"
-apt-get update > /dev/null
-apt-get install -y build-essential python3-dev python3-pip curl automake cmake git flex bison libglib2.0-dev libpixman-1-dev python3-setuptools libgtk-3-dev > /dev/null
-apt-get install -y lld llvm llvm-dev clang > /dev/null
-apt-get install -y gcc-$(gcc --version|head -n1|sed 's/\..*//'|sed 's/.* //')-plugin-dev libstdc++-$(gcc --version|head -n1|sed 's/\..*//'|sed 's/.* //')-dev > /dev/null
-git clone https://github.com/AFLplusplus/AFLplusplus > /dev/null
-cd AFLplusplus > /dev/null 
-make source-only NO_NYX=1 > /dev/null
+apt-get update
+apt-get install -y build-essential python3-dev python3-pip curl automake cmake git flex bison libglib2.0-dev libpixman-1-dev python3-setuptools libgtk-3-dev
+apt-get install -y lld llvm llvm-dev clang
+apt-get install -y gcc-$(gcc --version|head -n1|sed 's/\..*//'|sed 's/.* //')-plugin-dev libstdc++-$(gcc --version|head -n1|sed 's/\..*//'|sed 's/.* //')-dev
+git clone https://github.com/AFLplusplus/AFLplusplus
+cd AFLplusplus 
+make source-only NO_NYX=1
 make install 
 
 echo "- Installing AFL-utils"
-cd ~ > /dev/null
-git clone https://github.com/mimicria/afl-utils.git > /dev/null
+cd ~
+git clone https://github.com/mimicria/afl-utils.git
 cd afl-utils && python3 -m pip install twitter && python3 setup.py install
-cd ~ > /dev/null
-rm -rf afl-utils > /dev/null
+cd ~
+rm -rf afl-utils
 
 echo "- Installing AFL-cov"
-cd ~ > /dev/null
-git clone https://github.com/mimicria/afl-cov.git > /dev/null
-mv afl-cov /opt/afl-cov > /dev/null
+cd ~
+git clone https://github.com/mimicria/afl-cov.git
+mv afl-cov /opt/afl-cov
 # chmod ?
 ln -s /opt/afl-cov/afl-cov /bin/afl-cov
 
 echo "- Installing Fuzzman"
-cd ~ > /dev/null
-git clone https://github.com/mimicria/fuzzaide.git > /dev/null
+cd ~
+git clone https://github.com/mimicria/fuzzaide.git
 cd fuzzaide && pip install . 
-cd ~ > /dev/null
-rm -rf fuzzaide > /dev/null
+cd ~
+rm -rf fuzzaide
 
 echo "- Installing Casr"
 PATH=$PATH:/root/.cargo/bin
-cd ~ > /dev/null
-curl https://sh.rustup.rs | sh -s -- -y --default-toolchain=nightly --profile=minimal > /dev/null
+cd ~
+curl https://sh.rustup.rs | sh -s -- -y --default-toolchain=nightly --profile=minimal
 cargo install casr
