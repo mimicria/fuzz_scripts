061eaf52ec13cdc3cd0d6ce4727938a0cd6eecf7
mimicria <gmimicria@gmail.com>
2024-06-02 06:28:15 +0300

    Update aflplotter.py

--- aflplotter.py
+++ aflplotter.py
@@ -2,7 +2,8 @@
 
 import sys, os, subprocess
 
-path = sys.argv[1]
-dirlist = [f.path for f in os.scandir(path) if f.is_dir()]
-for i in dirlist:
-    subprocess.run(['afl-plot',i, i.replace('out','plot')])
+if len(sys.argv) > 1:
+    path = sys.argv[1]
+    dirlist = [f.path for f in os.scandir(path) if f.is_dir()]
+    for i in dirlist:
+        subprocess.run(['afl-plot', i, i.replace('out', 'plot')])
